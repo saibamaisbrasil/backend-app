eceef464b42d85b7fbc37071b8e91e716793c0cc
Daniel Silva <daniel.silva@resultsistemas.com>
2018-02-19 17:00:43 -0300

    alterado configuracao do cors

--- server.js
+++ server.js
@@ -16,26 +16,6 @@ app.use(cors());
 app.use(parser.json()); // support json encoded bodies
 app.use(parser.urlencoded({ extended: true })); // support encoded bodies
 
-// Add headers
-app.use(function (req, res, next) {
-
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
-});
-
 // registra as rotas com o prefixo api
 app.use('/api', routes());
 
